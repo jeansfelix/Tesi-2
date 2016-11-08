151c3d198b1775a2daed317f1397068861dba2ca
Jean Felix <jean.s.felix@gmail.com>
2016-11-08 00:39:52 -0200

    Contando palavras em cada episódio e contando palavras em todos os episódios.

--- trabalhos/tfidf.py
+++ trabalhos/tfidf.py
@@ -7,8 +7,6 @@ from os.path import isfile, isdir, join, basename, exists, abspath
 reload(sys)
 sys.setdefaultencoding("utf-8")
 
-stopWords = set(nltk.corpus.stopwords.words('english'))
-
 def main():
     if len(sys.argv) != 2:
         print "Deve ser passado o nome do diretorio PreProcessado."
@@ -33,15 +31,46 @@ def executarProcessamento(diretorioTemporadas):
 
 def processarEpisodios(caminhoEpisodios, episodios):
 
+    mapa_tf_episodioNumPalavras = dict()
     for episodio in episodios:
         arquivo = open(join(caminhoEpisodios, episodio))
-        textoArquivo = arquivo.read();
+        textoArquivo = arquivo.read().decode('utf8');
         arquivo.close()
 
+        stopWords = set(nltk.corpus.stopwords.words('english'))
+        #print stopWords
+
+        textoArquivo = re.sub(r'''[.,;:!?'"()]''', r' ', textoArquivo)
         palavras = [a for a in textoArquivo.lower().split() if a not in stopWords]
+        n_palavrasDistintas = len(set(palavras))
 
-        print palavras
+        print n_palavrasDistintas
 
+        tf = dict()
+        for palavra in palavras:
+            if palavra in tf:
+                tf[palavra] += 1.0
+            else:
+                tf[palavra] = 1.0
+
+        mapa_tf_episodioNumPalavras[episodio] = [tf, n_palavrasDistintas];
+
+    palavrasDistintas = []
+    for episodio in mapa_tf_episodioNumPalavras.iterkeys():
+        palavrasDistintas += mapa_tf_episodioNumPalavras[episodio][0].iterkeys()
+
+    palavrasDistintas = set(palavrasDistintas)
+
+    mapaNumAparicoesDaPalavra = dict()
+    for palavra in palavrasDistintas:
+        for episodio in mapa_tf_episodioNumPalavras.iterkeys():
+            if palavra in mapa_tf_episodioNumPalavras[episodio][0].iterkeys():
+                if palavra in mapaNumAparicoesDaPalavra:
+                    mapaNumAparicoesDaPalavra[palavra] += mapa_tf_episodioNumPalavras[episodio][0][palavra]
+                else:
+                    mapaNumAparicoesDaPalavra[palavra] = 1.0
+
+    print mapaNumAparicoesDaPalavra
 
 if __name__ == "__main__":
     main()
